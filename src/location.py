5ee3db48156975a427a7712c4274712fac2e5b23
Ori Shamir <orishamir04@gmail.com>
2023-04-27 22:04:41 +0300

    Validate location of stuff

--- src/location.py
+++ src/location.py
@@ -13,8 +13,8 @@ class Location(EnumMeta):
 
     LEFT_PANEL = r"HKEY_CURRENT_USER\Software\Classes\directory\Background\shell"
     LEFT_PANEL_ADMIN = r"HKEY_CLASSES_ROOT\Directory\Background\shell"
-    RIGHT_PANEL = r"HKEY_CURRENT_USER\Software\Classes\directory\shell"
-    RIGHT_PANEL_ADMIN = r"HKEY_CLASSES_ROOT\Directory\shell"
+    RIGHT_PANEL = r"HKEY_CURRENT_USER\Software\Classes\directory\Background\shell"
+    RIGHT_PANEL_ADMIN = r"HKEY_CLASSES_ROOT\Directory\Background\shell"
     FILE = r"HKEY_CURRENT_USER\Software\Classes\*\shell"
     FILE_ADMIN = r"HKEY_CLASSES_ROOT\*\shell"
     DESKTOP = r"HKEY_CLASSES_ROOT\DesktopBackground\Shell"
