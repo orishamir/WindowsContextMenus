6e0641a5d1cbf3a192578574ee91fbf5735b0d60
Ori Shamir <orishamir04@gmail.com>
2023-05-18 19:23:02 +0300

    Better __init__.py for conditions that includes whats useful.

--- src/conditions/__init__.py
+++ src/conditions/__init__.py
@@ -1,1 +1,8 @@
-from .icondition import ICondition
+from src.conditions.base_class.icondition import ICondition
+from .extension_type import ExtensionType
+from .base_class import (
+    ConditionBase,
+    And,
+    Or,
+    Not
+)
