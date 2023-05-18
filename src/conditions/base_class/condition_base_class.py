b2cf74c6b2428c5d3af35d13f32bffc05da702fd
Ori Shamir <orishamir04@gmail.com>
2023-05-18 19:19:24 +0300

    Add ConditionBase class for conditions to inherit from

---
+++ src/conditions/base_class/condition_base_class.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from src.conditions.base_class.icondition import ICondition
+from src.conditions.base_class.condition_meta import ConditionMeta
+
+AQS_OPERATOR_AND = " AND "
+AQS_OPERATOR_OR = " OR "
+AQS_OPERATOR_NOT = "NOT"
+
+
+class ConditionBase(metaclass=ConditionMeta):
+    """
+    This base class extends the ICondition interface by adding
+    definition for &, |, etc.
+    It also makes that every class that inherits from it
+    is automatically implementing ==, !=, etc. as a way to initialize it.
+
+    For example:
+        if ExtensionType inherits from ConditionBase,
+        then that means:
+            (ExtensionType != ".exe.")
+            is equivalent to
+            ExtensionType(NotEqual(".exe"))
+        and that
+            ExtensionType(NotEqual(".exe")) & ExtensionType(NotEqual(".dll"))
+            is equivalent to
+            And(
+                ExtensionType(NotEqual(".exe")),
+                ExtensionType(NotEqual(".dll"))
+            )
+    """
+
+    def __and__(self: ICondition, other: ICondition) -> ICondition:
+        return And(self, other)
+
+    def __rand__(self: ICondition, other: ICondition) -> ICondition:
+        return And(other, self)
+
+    def __or__(self: ICondition, other: ICondition) -> ICondition:
+        return Or(self, other)
+
+    def __ror__(self: ICondition, other: ICondition) -> ICondition:
+        return Or(other, self)
+
+    def __invert__(self: ICondition) -> ICondition:
+        return Not(self)
+
+
+class And(ConditionBase):
+    def __init__(self, *conditions: ICondition):
+        self.conditions = conditions
+
+    def to_aqs_string(self) -> str:
+        conditions_as_str = (condition.to_aqs_string() for condition in self.conditions)
+        chained_ands = AQS_OPERATOR_AND.join(conditions_as_str)
+        return f"({chained_ands})"
+
+
+class Or(ConditionBase):
+    def __init__(self, *conditions: ICondition):
+        self.conditions = conditions
+
+    def to_aqs_string(self) -> str:
+        conditions_as_str = (condition.to_aqs_string() for condition in self.conditions)
+        chained_ors = AQS_OPERATOR_OR.join(conditions_as_str)
+        return f"({chained_ors})"
+
+
+class Not(ConditionBase):
+    def __init__(self, condition: ICondition):
+        self.condition = condition
+
+    def to_aqs_string(self) -> str:
+        return f"({AQS_OPERATOR_NOT} {self.condition.to_aqs_string()})"
---
+++ src/conditions/base_class/condition_meta.py
@@ -0,0 +1,33 @@
+from src.conditions.operators import Equal, NotEqual, LessThan, LessThanEqual, GreaterThan, GreaterThanEqual
+from src.conditions.base_class.icondition import ICondition
+
+
+class ConditionMeta(type):
+    """
+    This is the metaclass for conditions, that's for the
+    `==` or `!=` or `<=` functionality to work.
+
+    For example:
+    if ExtensionType's metaclass is ConditionBase, then that means:
+        (ExtensionType != ".exe.")
+    works and is equivalent to
+        ExtensionType(NotEqual(".exe"))
+    """
+
+    def __eq__(cls, other: str | int | float) -> ICondition:
+        return cls(Equal(other))
+
+    def __ne__(cls, other: str | int | float) -> ICondition:
+        return cls(NotEqual(other))
+
+    def __lt__(cls, other: str | int | float) -> ICondition:
+        return cls(LessThan(other))
+
+    def __le__(cls, other: str | int | float) -> ICondition:
+        return cls(LessThanEqual(other))
+
+    def __gt__(cls, other: str | int | float) -> ICondition:
+        return cls(GreaterThan(other))
+
+    def __ge__(cls, other: str | int | float) -> ICondition:
+        return cls(GreaterThanEqual(other))
