8f48bfeecd26577728f765170ee6b6e437e01297
Ori Shamir <orishamir04@gmail.com>
2023-05-18 19:21:41 +0300

    Add first Condition - ExtensionType

---
+++ src/conditions/extension_type.py
@@ -0,0 +1,26 @@
+from src.conditions.base_class.condition_base_class import ConditionBase
+from src.conditions.operators import IOperator, Equal
+
+PROPERTY_NAME = "System.FileExtension"
+
+
+class ExtensionType(ConditionBase):
+    """
+    This condition checks the value of the extension of the file.
+    """
+
+    def __init__(self, ext: str | IOperator):
+        if isinstance(ext, str):
+            ext = Equal(ext)
+
+        if not isinstance(ext, IOperator):
+            raise ValueError("Extension should either be a string or an operator.")
+
+        self.ext = ext
+
+    def to_aqs_string(self) -> str:
+        """
+        Convert condition to its AQS representation.
+        """
+
+        return f"({PROPERTY_NAME}{self.ext.to_aqs_string()})"
