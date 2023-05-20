b9c082422da805631992a2f5e89937502ca33d82
Ori Shamir <orishamir04@gmail.com>
2023-05-20 16:32:49 +0300

    Add example usage of main

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ https://learn.microsoft.com/en-us/windows/win32/shell/context-menu-handlers
 
 https://learn.microsoft.com/en-us/previous-versions//ff521735(v=vs.85)
 """
-from src.conditions import ExtensionType
+from src.conditions import ExtensionType, DateModified
 from src.location import Location
 from src.registry_interaction import apply_context_menu
 from src.context_menu import ContextMenu
-from src.features import Command, EntryName, Icon, ShiftClick, Condition
+from src.features import (
+    Command,
+    EntryName,
+    Icon,
+    ShiftClick,
+    Condition
+)
 
 main: ContextMenu = ContextMenu(
     "SearchGoogle",
@@ -15,13 +21,12 @@ main: ContextMenu = ContextMenu(
         EntryName("Search in google"),
         Icon(r'C:\Program Files\Google\Chrome\Application\chrome.exe'),
         ShiftClick(),
-        Command(r'"cmd.exe" /c start chrome www.google.com/search?q="%V"'),
+        Command(r'python -m '),
         Condition(
-            (ExtensionType != ".exe") & (ExtensionType != ".dll") & (ExtensionType != ".pdf")
+            (ExtensionType == ".proto") | (ExtensionType == ".proto3")
         )
     ]
 )
-
 
 if __name__ == '__main__':
     apply_context_menu(
