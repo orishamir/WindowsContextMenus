a02821943d46ca573173a862c15fef3533611080
Ori Shamir <orishamir04@gmail.com>
2025-01-12 21:35:33 +0200

    Document dos wildcard comparer

--- src/comparers/dos_wildcard.py
+++ src/comparers/dos_wildcard.py
@@ -2,6 +2,14 @@ from src.comparers.icomparer import IComparer
 
 
 class DosWildcard(IComparer):
+    """
+    Works like DOS-style wildcard characters:
+        ? matches one arbitrary character.
+        * matches zero or more arbitrary characters.
+    Example:
+        "Mic?osoft W*d" - Finds files where the file name starts with "Mic", followed by some character,
+                          followed by "osoft w", followed by any characters ending with d.
+    """
     def __init__(self, string: str):
         if not isinstance(string, str):
             raise TypeError(f"Value must be str. Type is {type(string)}")
