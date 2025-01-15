199f4198bcd03af760adb88d0e651396a24b0c81
Ori Shamir <orishamir04@gmail.com>
2025-01-15 23:24:28 +0200

    Add FastAPI endpoint

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ https://learn.microsoft.com/en-us/windows/win32/shell/context-menu-handlers
 
 https://learn.microsoft.com/en-us/previous-versions//ff521735(v=vs.85)
 """
-from enum import StrEnum
+import uvicorn
 
-from src.conditions import ExtensionType
-from src.context_menu_locations import ContextMenuLocation
-from src.registry_interaction import apply_context_menu
-from src.context_menu import ContextMenu
-from src.features import EntryName as EntryName1
-from features import EntryName as EntryName2
+from src.api.make_reg_file import app
 
-print(EntryName1 == EntryName2)
+uvicorn.run(
+    app=app,
+)
