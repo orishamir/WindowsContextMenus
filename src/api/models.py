87fa8390629714ddf9295c65397612e640daf195
Ori Shamir <orishamir04@gmail.com>
2025-01-15 23:23:01 +0200

    Add context menu and conditions config models

---
+++ src/api/models.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel, Field, ByteSize
+
+from src.api.comparers import FileNameComparerConfig, FileSizeComparerConfig, ExtensionComparerConfig
+
+
+class ContextMenuConfig(BaseModel):
+    display_name: str
+    exec: str
+    icon: Optional[Path] = None
+    disabled: Optional[bool] = None
+    on_shift_click: Optional[bool] = None
+    conditions: Optional[ConditionsConfig] = None
+    sub_menus: list[ContextMenuConfig] = []
+
+
+class ConditionsConfig(BaseModel):
+    file_name: Optional[str | FileNameComparerConfig] = None
+    file_size: Optional[ByteSize | FileSizeComparerConfig] = None
+    extension: Optional[str | ExtensionComparerConfig] = None
+
+    or_: Optional[list[ConditionsConfig]] = Field(None, alias="$or")
+    and_: Optional[list[ConditionsConfig]] = Field(None, alias="$and")
+    not_: Optional[ConditionsConfig] = Field(None, alias="$not")
