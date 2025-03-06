e616fb2d6c98760921df59ba6c76de2acd55e6d3
Ori Shamir <orishamir04@gmail.com>
2025-03-07 00:04:48 +0200

    use new comparison type for conditions

--- context_menu_toolkit/conditions/extension_type.py
+++ context_menu_toolkit/conditions/extension_type.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
-from context_menu_toolkit.comparers import IComparer
 from context_menu_toolkit.conditions.base_class.condition_base_class import ConditionBase
+from context_menu_toolkit.conditions.comparison_type import ComparisonType
 
 PROPERTY_NAME = "System.FileExtension"
 
@@ -13,7 +13,8 @@ class ExtensionType(ConditionBase):
     Reference:
         [MSDN - System.FileExtension](https://learn.microsoft.com/en-us/windows/win32/properties/props-system-fileextension)
     """
-    comparer: IComparer
+    extension: str
+    comparison: ComparisonType
 
     def to_aqs_string(self) -> str:
-        return f"({PROPERTY_NAME}{self.comparer.to_aqs_string()})"
+        return f"({PROPERTY_NAME}:{self.comparison}{self.extension})"
--- context_menu_toolkit/conditions/file_size.py
+++ context_menu_toolkit/conditions/file_size.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
-from context_menu_toolkit.comparers import IComparer
+from pydantic import ByteSize
+
 from context_menu_toolkit.conditions.base_class.condition_base_class import ConditionBase
+from context_menu_toolkit.conditions.comparison_type import ComparisonType
 
 PROPERTY_NAME = "System.Size"
 
@@ -19,7 +21,13 @@ class FileSize(ConditionBase):
     References:
         [MSDN - System.Size](https://learn.microsoft.com/en-us/windows/win32/properties/props-system-size)
     """
-    comparer: IComparer
+    size: ByteSize
+    comparison: ComparisonType
 
     def to_aqs_string(self) -> str:
-        return f"({PROPERTY_NAME}{self.comparer.to_aqs_string()})"
+        """Convert file size to Advanced Query Syntax representation.
+
+        Example:
+            "System.Size:<30MB"
+        """
+        return f"({PROPERTY_NAME}:{self.comparison}{self.size.human_readable(decimal=True)})"
