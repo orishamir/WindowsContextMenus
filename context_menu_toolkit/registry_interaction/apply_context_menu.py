12cb29bfb016ec2e8e686249a3d2a013b949152a
Ori Shamir <orishamir04@gmail.com>
2025-03-07 14:03:24 +0200

    create registry path helper class

--- context_menu_toolkit/registry_interaction/apply_context_menu.py
+++ context_menu_toolkit/registry_interaction/apply_context_menu.py
@@ -2,10 +2,18 @@ import winreg
 
 from context_menu_toolkit.context_menu import ContextMenu
 from context_menu_toolkit.context_menu_locations import ContextMenuLocation
-from context_menu_toolkit.registry_interaction.registry_location import _RegistryLocation
 from context_menu_toolkit.registry_structs import RegistryKey
+from context_menu_toolkit.registry_structs.registry_path import RegistryPath, TopLevelKey
 
-RESERVED = 0
+RESERVED_FLAG = 0
+
+_TOP_LEVEL_KEY_TO_VALUE: dict[TopLevelKey, int] = {
+    TopLevelKey.HKEY_CLASSES_ROOT: winreg.HKEY_CLASSES_ROOT,
+    TopLevelKey.HKEY_CURRENT_USER: winreg.HKEY_CURRENT_USER,
+    TopLevelKey.HKEY_LOCAL_MACHINE: winreg.HKEY_LOCAL_MACHINE,
+    TopLevelKey.HKEY_USERS: winreg.HKEY_USERS,
+    TopLevelKey.HKEY_CURRENT_CONFIG: winreg.HKEY_CURRENT_CONFIG,
+}
 
 
 def apply_context_menu(
@@ -15,12 +23,12 @@ def apply_context_menu(
     """Apply context menu to the registry at `location`."""
     built_menu: RegistryKey = menu.build()
 
-    _apply_registry_key(built_menu, _RegistryLocation.from_string(location))
+    _apply_registry_key(built_menu, RegistryPath(location))
 
     return f"{location}\\{menu.name}"
 
 
-def _apply_registry_key(key: RegistryKey, location: _RegistryLocation) -> None:
+def _apply_registry_key(key: RegistryKey, location: RegistryPath) -> None:
     new_loc = location / key.name
 
     _create_key(new_loc)
@@ -35,10 +43,20 @@ def _apply_registry_key(key: RegistryKey, location: _RegistryLocation) -> None:
         _apply_registry_key(subkey, new_loc)
 
 
-def _create_key(location: _RegistryLocation) -> None:
-    winreg.CloseKey(winreg.CreateKey(location.top_level, location.subkey))
+def _create_key(location: RegistryPath) -> None:
+    winreg.CloseKey(
+        winreg.CreateKey(
+            _TOP_LEVEL_KEY_TO_VALUE[location.top_level_key],
+            location.subkeys,
+        ),
+    )
 
 
-def _set_value(location: _RegistryLocation, value_name: str, value_type: int, data: str | int) -> None:
-    with winreg.OpenKey(location.top_level, location.subkey, RESERVED, winreg.KEY_SET_VALUE) as key:
-        winreg.SetValueEx(key, value_name, RESERVED, value_type, data)
+def _set_value(location: RegistryPath, value_name: str, value_type: int, data: str | int) -> None:
+    with winreg.OpenKey(
+        _TOP_LEVEL_KEY_TO_VALUE[location.top_level_key],
+        location.subkeys,
+        RESERVED_FLAG,
+        winreg.KEY_SET_VALUE,
+    ) as key:
+        winreg.SetValueEx(key, value_name, RESERVED_FLAG, value_type, data)
