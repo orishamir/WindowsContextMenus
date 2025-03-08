043fea634b929a4aab4682db77d45a369d94c036
Ori Shamir <orishamir04@gmail.com>
2025-03-08 18:51:31 +0200

    add todo instructions

---
+++ context_menu_toolkit/register_custom_filetype.py
@@ -0,0 +1,6 @@
+r"""Add support for custom filetype registration.
+
+TODO: Look at HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.py, HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.pyd, etc.
+      that is how Python registers custom handlers.
+      Then, in HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Python.CompiledFile they dictate the actual menu.
+"""
