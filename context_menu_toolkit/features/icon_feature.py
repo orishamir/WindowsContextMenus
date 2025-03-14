eafb782a977480cbf5dbc6abefa271d74123e721
Ori Shamir <orishamir04@gmail.com>
2025-03-14 11:22:52 +0200

    add WindowsIcon enum

--- context_menu_toolkit/features/icon_feature.py
+++ context_menu_toolkit/features/icon_feature.py
@@ -1,13 +1,10 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
-from pathlib import Path
+from enum import StrEnum
 
 from context_menu_toolkit.features.ifeature import IFeature
 from context_menu_toolkit.registry_structs import DataType, RegistryKey, RegistryValue
-
-_ACCEPTED_ICON_EXTENSIONS = (
-    ".ico",
-    ".exe",
-)
 
 
 @dataclass
@@ -15,17 +12,67 @@ class Icon(IFeature):
     """Add an icon that will be displayed for the context menu.
 
     Attributes:
-        path_to_icon: Path on the local machine for the icon.
+        path_to_icon: Windows dll/exe icon, or a path on the local machine for the icon.
     """
-    path_to_icon: str
+    path_to_icon: WindowsIcon | str
 
     def apply_to(self, tree: RegistryKey) -> None:
         tree.values.append(
             RegistryValue(name="Icon", type=DataType.REG_SZ, data=str(self.path_to_icon)),
         )
 
-    def __post_init__(self) -> None:
-        if Path(self.path_to_icon).suffix not in _ACCEPTED_ICON_EXTENSIONS:
-            raise ValueError(
-                f"Icon not in acceptable extensions: {self.path_to_icon}. Available extensions: {_ACCEPTED_ICON_EXTENSIONS}",
-            )
+
+class WindowsIcon(StrEnum):
+    r"""A very limited list of possible values for icons.
+
+    This is only a very partial list.
+    For exploring more options, download [IconExtract](https://www.nirsoft.net/utils/iconsext.html) (Bottom of the page),
+    open the wanted dll/exe, for example C:\Windows\System32\imageres.dll.
+    You'll see a list of icons, with a number in parentheses.
+    To use that icon, path_to_icon="imageres.dll,-{icon_number}".
+    Note the negative sign before the icon number.
+
+    Tip:
+        The DLL "imageres.dll" is highly recommended.
+
+    Notice:
+        Sometimes a negative icon number won't work, instead you'll need to use icon_number-1 (the actual index).
+
+    Notice:
+        It is possible that a newer, more modern icon exists for the icon you're looking for.
+        For example "shell32.dll,-322" is the newer "ieframe.dll,19".
+
+    [^1]: <https://www.urtech.ca/2022/07/solved-download-list-of-icons-in-shell32-dll/amp>
+    [^2]: <https://www.digitalcitizen.life/where-find-most-windows-10s-native-icons>
+    [^3]: <https://diymediahome.org/windows-icons-reference-list-with-details-locations-images> - Older icons
+    """
+    THIS_PC = "shell32.dll,-16"
+    DOCUMENTS_FOLDER = "shell32.dll,-235"
+    PICTURES_FOLDER = "shell32.dll,-236"
+    MUSIC_FOLDER = "shell32.dll,-237"
+    VIDEOS_FOLDER = "shell32.dll,-238"
+    HOME = "shell32.dll,-51380"
+    DOWNLOADS_FOLDER = "imageres.dll,-184"
+    DESKTOP_FOLDER = "imageres.dll,-183"
+
+    UNDO = "imageres.dll,-5315"
+    REDO = "imageres.dll,-5311"
+    USER_ACCOUNT_CONTROL_UAC = "imageres.dll,-78"
+    """Also see HasLuaShield feature."""
+
+    CLOUD = "imageres.dll,-1404"
+
+    YELLOW_PADLOCK = "shell32.dll,-48"
+    CONTROL_PANEL = "shell32.dll,-22"
+    EMPTY_TRASH_BIN = "shell32.dll,-32"
+    FULL_TRASH_BIN = "shell32.dll,-33"
+    BLUE_PLAY_BUTTON = "shell32.dll,-246"
+
+    RED_FORBIDDEN = "shell32.dll,-200"
+    RED_X = "shell32.dll,-220"
+    RED_CHECKMARK = "shell32.dll,-253"
+    GREEN_CHECKMARK = "shell32.dll,-16802"
+    GREEN_CHECKMARK_BOXED = "imageres.dll,-1400"
+    REFRESH = "imageres.dll,-1401"
+
+    CMD = "cmd.exe"
