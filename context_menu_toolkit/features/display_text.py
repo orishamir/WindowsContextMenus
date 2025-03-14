b801d912e2d60e4e39af3d82f5ca36b1b4f89463
Ori Shamir <orishamir04@gmail.com>
2025-03-14 16:37:00 +0200

    rename DisplayText attribute to text

--- context_menu_toolkit/features/display_text.py
+++ context_menu_toolkit/features/display_text.py
@@ -9,13 +9,13 @@ class DisplayText(IFeature):
     """Defines the displayed text of the context menu, i.e. the text that appears on right click.
 
     Attributes:
-        name: The text that should be displayed for the context menu on right-click.
+        text: The text that should be displayed for the context menu on right-click.
               You can also use string localization, which tells Windows to look up a string resource from a DLL (or EXE) file.
               For example `MUIVerb=@shell32.dll,-8518` -> `"Send To"`
     """
-    name: str
+    text: str
 
     def apply_to(self, tree: RegistryKey) -> None:
         tree.values.append(
-            RegistryValue(name="MUIVerb", type=DataType.REG_SZ, data=self.name),
+            RegistryValue(name="MUIVerb", type=DataType.REG_SZ, data=self.text),
         )
