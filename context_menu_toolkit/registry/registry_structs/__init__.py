613af1d5b23a7dfd92939d03684f272e4fd6282f
Ori Shamir <orishamir04@gmail.com>
2025-04-06 21:52:34 +0300

    Reformat src

--- context_menu_toolkit/registry/registry_structs/__init__.py
+++ context_menu_toolkit/registry/registry_structs/__init__.py
@@ -3,6 +3,7 @@
 References:
     MSDN | Structure of the Registry(https://learn.microsoft.com/en-us/windows/win32/sysinfo/structure-of-the-registry)
 """
+
 from .registry_key import RegistryKey
 from .registry_path import RegistryPath, TopLevelKey
 from .registry_value import DataType, RegistryValue
